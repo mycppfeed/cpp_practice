5c1747771bad92ab50800a51bca999087cd8e2b0
Marq <marq@blakfx.com>
2020-05-28 18:44:18 +0530

    More General constructors

--- gen/constructors.cpp
+++ gen/constructors.cpp
@@ -53,8 +53,85 @@ void test_constructors() {
     std::cout << "Set After emplace : " << points;
 }
 
+class Point0D {
+public:
+    Point0D() = default;
+    int x;
+};
+
+class Point1D {
+public:
+    Point1D() = default;
+    int x{};
+};
+
+class Point2D {
+public:
+    // Enabling below will need us to implement it
+    // Point2D();
+
+    // Point2D() {} // One of potential empty implementation
+    int x;
+};
+
+class Point3D {
+public:
+    int x{};
+};
+
+class Point4D {
+public:
+    Point4D() { }
+    int x;
+};
+
+class Point5D {
+public:
+    Point5D() { }
+    int x{};
+};
+
+void test_initializations() {
+    Point0D point0Dreg;
+    Point0D point0Duni{};
+
+    printf("0D Reg : X = %d\n", point0Dreg.x);
+    printf("0D Uni : X = %d\n\n", point0Duni.x);
+
+    Point1D point1Dreg;
+    Point1D point1Duni{};
+
+    printf("1D Reg : X = %d\n", point1Dreg.x);
+    printf("1D Uni : X = %d\n\n", point1Duni.x);
+
+    Point2D point2Dreg;
+    Point2D point2Duni{};
+
+    printf("2D Reg : X = %d\n", point2Dreg.x);
+    printf("2D Uni : X = %d\n\n", point2Duni.x);
+
+    Point3D point3Dreg;
+    Point3D point3Duni{};
+
+    printf("3D Reg : X = %d\n", point3Dreg.x);
+    printf("3D Uni : X = %d\n\n", point3Duni.x);
+
+    Point4D point4Dreg;
+    Point4D point4Duni{};
+
+    printf("4D Reg : X = %d\n", point4Dreg.x);
+    printf("4D Uni : X = %d\n\n", point4Duni.x);
+
+    Point5D point5Dreg;
+    Point5D point5Duni{};
+
+    printf("5D Reg : X = %d\n", point5Dreg.x);
+    printf("5D Uni : X = %d\n\n", point5Duni.x);
+}
+
 int main() {
     test_constructors();
+    test_initializations();
 
     return 0;
 }
