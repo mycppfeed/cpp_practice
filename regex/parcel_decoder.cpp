c876a7a0d7901816b31c3028eb74e0ee170c4c96
User <user@amsterdam.lan>
2020-06-08 15:22:15 +0530

    Improved regex with lambda and added mobile data

--- regex/parcel_decoder.cpp
+++ regex/parcel_decoder.cpp
@@ -9,25 +9,6 @@
 // 43 4f 4e 4e 45 43 54 45 44 00
 // 4f 43 4e 4e 43 45 45 54 00 44
 
-/*
- * 00000000   00000001   00000001   00000000
- *
- * 0000 0000  0000 0001  0000 0001  0000 0000
- *
- *
- * // Interchange
- * 0000 0000  0001 0000  0001 0000  0000 0000
- *
- * // Stripping
- * 0000 0001   0001 0000
- *
- * // stripping
- * 00   00    00   01    00   01    00   00
- *
- * // Interchange
- * 00   00    01  00     01   00    00   00
- */
-
  /*
   *
   * 00490057   00490046
@@ -43,69 +24,72 @@
   * W  I    F  I
   **/
 
-std::string wifiData = "Result: Parcel( \n\
-    0x00000000: 00000000 00000001 00000001 00000000  '................'\n\
-    0x00000010: 00000004 00490057 00490046 00000000  '................'\n\
-    0x00000020: 00000000 00000000 00000009 004f0043  '................'\n\
-    0x00000030: 004e004e 00430045 00450054 00000044  '................'\n\
-    0x00000040: 00000009 004f0043 004e004e 00430045  '................'\n\
-    0x00000050: 00450054 00000044 00000000 00000001  '................'\n\
-    0x00000060: 00540043 00440045 00000000 00000000  '................'\n\
-    0x00000070: 00000000 ffffffff ffffffff 00000000  '................'\n\
-    0x00000080: 00000000 00000000 00000000 00000000  '................'\n\
-    0x00000090: 00000000 00000000 00000000 00000000  '................'\n\
-    0x000000a0: 00000000 00000000 00000000 00000000  '................')\n";
+static std::string mobileData = "Result: Parcel( \n"
+    "    0x00000000: 00000000 00000001 00000000 00000000  '................'\n"
+    "    0x00000010: 00000006 004f004d 00490042 0045004c  '................'\n"
+    "    0x00000020: 00000000 00000000 00000000 0000000c  '................'\n"
+    "    0x00000030: 00490044 00430053 004e004f 0045004e  '................'\n"
+    "    0x00000040: 00540043 00440045 00000000 0000000c  '................'\n"
+    "    0x00000050: 00490044 00430053 004e004f 0045004e  '................'\n"
+    "    0x00000060: 00540043 00440045 00000000 00000000  '................'\n"
+    "    0x00000070: 00000001 00000000 ffffffff ffffffff  '................'\n"
+    "    0x00000080: 00000000 00000000 00000000 00000000  '................'\n"
+    "    0x00000090: 00000000 00000000 00000000 00000000  '................'\n"
+    "    0x000000a0: 00000000 00000000 00000000 00000000  '................'\n"
+    "    0x000000b0: 00000000                             '....            ')\n";
+
+static std::string wifiData = "Result: Parcel( \n"
+    "    0x00000000: 00000000 00000001 00000001 00000000  '................'\n"
+    "    0x00000010: 00000004 00490057 00490046 00000000  '................'\n"
+    "    0x00000020: 00000000 00000000 00000009 004f0043  '................'\n"
+    "    0x00000030: 004e004e 00430045 00450054 00000044  '................'\n"
+    "    0x00000040: 00000009 004f0043 004e004e 00430045  '................'\n"
+    "    0x00000050: 00450054 00000044 00000000 00000001  '................'\n"
+    "    0x00000060: 00540043 00440045 00000000 00000000  '................'\n"
+    "    0x00000070: 00000000 ffffffff ffffffff 00000000  '................'\n"
+    "    0x00000080: 00000000 00000000 00000000 00000000  '................'\n"
+    "    0x00000090: 00000000 00000000 00000000 00000000  '................'\n"
+    "    0x000000a0: 00000000 00000000 00000000 00000000  '................')\n";
 
 int main() {
-    std::regex eightHexDigits_pattern("\\b[a-f0-9]{8}\\b");
-//    auto rgx_itr_end = std::sregex_iterator();
 
-    auto eightHex_start = std::sregex_iterator(wifiData.begin(), wifiData.end(), eightHexDigits_pattern);
+    auto GetLength = [](std::sregex_iterator& it) {
+        return std::stoul((*it++).str(), nullptr, 16);
+    };
 
-    std::advance(eightHex_start, 4);
-    std::smatch typeName_match = *eightHex_start;
-    int typeName_len = std::stoul(typeName_match.str(), nullptr, 16);
-
-    auto GetTwoChars = [](std::string& str, std::string& result) {
+    auto ExtractString = [](const std::sregex_iterator& it, std::string& result) {
+        auto str = (*it).str();
         std::rotate(str.begin(), str.begin()+4, str.end());
         result += stoul(str.substr(0,4), nullptr, 16);
         result += stoul(str.substr(4,8), nullptr, 16);
     };
 
-    auto ExtractString = [GetTwoChars](const std::smatch& match, std::string& result) {
-        auto match_str = match.str();
-        GetTwoChars(match_str, result);
+    auto ExtractStringAndAdvance = [GetLength, ExtractString]
+    (std::sregex_iterator& it, std::string& result) {
+        size_t len = GetLength(it);
+        auto start = it;
+        std::advance(it, (len / 2) + 1);
+
+        for (auto i = start; i != it; i++)
+            ExtractString(i, result);
     };
 
-    auto typeName_start = ++eightHex_start;
-    auto typeName_end = typeName_start;
-    std::advance(typeName_end, (typeName_len/2)+1);
-    std::string typeName;
-    
-    for (auto i = typeName_start; i != typeName_end; i++) {
-        auto match = *i;
-        ExtractString(match, typeName);
-    }
-    std::cout << "TypeName : " << typeName << "\n\n";
+    auto CheckStatus = [ExtractStringAndAdvance](const std::string& blob) {
+        std::regex reg("\\b[a-f0-9]{8}\\b");
+        auto rgxIt = std::sregex_iterator(blob.begin(), blob.end(), reg);
+        std::advance(rgxIt, 4);
+        std::string Name;
+        ExtractStringAndAdvance(rgxIt, Name);
 
-    
-    
-    eightHex_start = typeName_end;
-    std::advance(eightHex_start, 2);
+        std::advance(rgxIt, 2);
+        std::string status;
+        ExtractStringAndAdvance(rgxIt, status);
 
-    std::smatch status_match = *eightHex_start;
-    int status_len = std::stoul(status_match.str(), nullptr, 16);
-    
-    auto status_start = ++eightHex_start;
-    auto status_end = status_start;
-    std::advance(status_end, (status_len/2)+1);
-    std::string status;
-    
-    for (auto i = status_start; i != status_end; i++) {
-        auto match = *i;
-        ExtractString(match, status);
-    }
-    std::cout << "status : " << status << "\n\n";
+        std::cout << " [ " << Name << " ] : " << status << "\n\n";
+    };
+
+    CheckStatus(mobileData);
+    CheckStatus(wifiData);
 
     return 0;
 }
